09f051f5efcc5cf68d96a7b828ffa4f494f07b05
Anosha Sohail <86046973+AnoshaSohail@users.noreply.github.com>
2023-12-21 05:06:55 +0000

    anosha

---
+++ 01_basics/05_conversionOperations.js
@@ -0,0 +1,39 @@
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  output => 12
+// console.log(1 + "2"); output => 12
+// console.log("1" + 2 + 2); output => 122
+// console.log(1 + 2 + "2"); output => 32
+//if 1st string then all other will be treated as string if number then 1st num then string
+// console.log( (3 + 4) * 5 % 3); output => 
+
+// console.log(+true); output => true is a boolean but when you incremented it then the type will be converted to number so the output will be 1
+// console.log(+""); output => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter; //prefix,postfix ..mdn js documentation
+console.log(gameCounter); //101
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,37 @@
+//  Primitive
+//call by value:memory ka ref ni dia jata original data copy kr k dia jata ha us mn jo b changes krty hain wo copy mn krty hain 
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n //bigint
+
+
+
+// Reference (Non primitive)//return type obj
+//memory mn in ka refrence ap ko directly allocate kia ja skta ha 
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
