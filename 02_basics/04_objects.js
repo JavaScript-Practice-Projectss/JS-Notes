d15e50317c74abcae884626afe21ee24e4f11c4f
Anosha Sohail <86046973+AnoshaSohail@users.noreply.github.com>
2024-01-04 05:12:17 +0000

    anosha

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,12 +1,9 @@
 // const tinderUser = new Object()
 const tinderUser = {}
-
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
-
 // console.log(tinderUser);
-
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
@@ -16,20 +13,14 @@ const regularUser = {
         }
     }
 }
-
 // console.log(regularUser.fullname.userfullname.firstname);
-
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
-
 // const obj3 = { obj1, obj2 }//{{1: "a", 2: "b"},{3: "a", 4: "b"}}
  const obj3 = Object.assign({}, obj1, obj2, obj4)
-
 //const obj3 = {...obj1, ...obj2}
  console.log(obj3);
-
-
 const users = [
     {
         id: 1,
@@ -47,33 +38,26 @@ const users = [
 
 users[1].email
 // console.log(tinderUser);
-
 // console.log(Object.keys(tinderUser));//returns all keys of the object in array
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
-
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
-
+//**************de-structure*****************
 const course = {
     coursename: "js in hindi",
     price: "999",
     courseInstructor: "hitesh"
 }
-
-// course.courseInstructor
-
+// course.courseInstructor //br br course. lagny ki bajaye hum const {courseInstructor} = course(obj ka name)
+//if you think k courseInstructor bht bara ha to apny hisab sy b dy skty i.e below courseInstructor: instructor
 const {courseInstructor: instructor} = course
-
 // console.log(courseInstructor);
 //console.log(instructor);
-
 // {
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
-
 [
     {},
     {},
