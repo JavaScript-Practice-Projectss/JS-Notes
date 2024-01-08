f9113022c2d6962fddc707d6884aa82e57daad7e
Anosha Sohail <86046973+AnoshaSohail@users.noreply.github.com>
2024-01-08 05:58:22 +0000

    loops

---
+++ 05_iterations/03_forOf.js
@@ -0,0 +1,40 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+//array jesy hi but thora iteration ka hota ha..unique values..jis order main enter kia usi mn rehti
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+//destructure kr dia [key, value] ..agr just key hota to key ko console.log krny pr each key array ki form mn ati
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }//will outout that the object is not iteratable 
