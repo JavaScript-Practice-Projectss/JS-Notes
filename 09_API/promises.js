15fe552285132299de93d146ba4c4ce319c84a70
Anosha Sohail <86046973+AnoshaSohail@users.noreply.github.com>
2024-01-29 06:30:32 +0000

    promises

--- 09_API/promises.js
+++ 09_API/promises.js
@@ -1,16 +1,20 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
+//Do an async task
     // DB calls, cryptography, network
+    //promises k 3 states hoty hain..pending , fullfil, rejected
+    //it is a object so to get a new instance of class i.e object we use new keyword so
+   // new Promise() ..it take a callback... function(resolve, reject)
+   //following is promise creation
+const promiseOne = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log('Async task is compelete');
         resolve()
     }, 1000)
 })
-
+//promise consumption..  .then ka direct connection ha ap k resolve k sth is k andr b ek callback milta ha ye automatically ap ko ek argument retuen krta ha ...jo b value ya kam promise mn huwa ha wo return hota ha  ...agr ap resolve() call ni krty promise mn to ap kapromise or .then connect hi ni hoty you've to connect'em
 promiseOne.then(function(){
     console.log("Promise consumed");
 })
-
+//at same place
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Async task 2");
@@ -23,12 +27,12 @@ new Promise(function(resolve, reject){
 
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
-        resolve({username: "Chai", email: "chai@example.com"})
+        resolve({username: "Chai", email: "chai@example.com"})//value pas to .then
     }, 1000)
 })
 
 promiseThree.then(function(user){
-    console.log(user);
+    console.log(user);//user..the data recieved from resolve()
 })
 
 const promiseFour = new Promise(function(resolve, reject){
@@ -46,11 +50,11 @@ const promiseFour = new Promise(function(resolve, reject){
  .then((user) => {
     console.log(user);
     return user.username
-}).then((username) => {
+}).then((username) => {//agr further data chiyay ho to chaining k skty ek or .then lga k
     console.log(username);
 }).catch(function(error){
     console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+}).finally(() => console.log("The promise is either resolved or rejected")) //end pr bta do k jo b kam krna tha wo ho gya 
 
 
 
@@ -64,10 +68,11 @@ const promiseFive = new Promise(function(resolve, reject){
         }
     }, 1000)
 });
-
+// zrori ni k hr bar promise .then sy hi kro 
+//async ..await///ek problem ha k directly error ko handel ni kr skty hain to hum try catch use krty
 async function consumePromiseFive(){
     try {
-        const response = await promiseFive
+        const response = await promiseFive //jo b value ho gi promise ki a jye gi
         console.log(response);
     } catch (error) {
         console.log(error);
@@ -80,7 +85,7 @@ consumePromiseFive()
 //     try {
 //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-//         const data = await response.json()
+//         const data = await response.json() //ye convert hony mn b tie lgta ha to is liay isy b await krwana prhy ga
 //         console.log(data);
 //     } catch (error) {
 //         console.log("E: ", error);
